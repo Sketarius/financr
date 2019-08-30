e7925040043305f9f0dbc088584a2e1000fc691f
Shane Freeman <sketarius@gmail.com>
2019-08-29 22:48:35 -0400

    Updating for public

--- www/unit_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../core/FinancrStore.php');
-
-$store = new FinancrStore();
-
-$res = $store->userCompletedFirstTimeSetup('sketarius@gmail.com');
-
-if($res) { echo "true!"; } else { echo "false"; }
-?>
